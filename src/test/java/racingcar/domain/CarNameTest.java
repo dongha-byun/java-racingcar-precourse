8ab5e185500df9f45daa4a938f107c693c5d6b16
dongha-byun <byunsw4@naver.com>
2022-10-09 18:00:51 +0900

    Refactor: CarName 객체 관련 유효성 검사 제거
    Utils class(CarNameParser.java) 에서 검증하도록 수정

--- src/test/java/racingcar/domain/CarNameTest.java
+++ src/test/java/racingcar/domain/CarNameTest.java
@@ -8,20 +8,6 @@ import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException
 public class CarNameTest {
 
     @Test
-    void 차_이름_길이_예외_테스트(){
-        assertThatIllegalArgumentException().isThrownBy(
-                () -> new CarName("dongHa")
-        );
-    }
-
-    @Test
-    void 띄어쓰기_예외_테스트(){
-        assertThatIllegalArgumentException().isThrownBy(
-                () -> new CarName("dong ")
-        );
-    }
-
-    @Test
     void 차_이름_생성_테스트(){
         CarName dong = new CarName("dong");
         assertThat(dong).isNotNull();
