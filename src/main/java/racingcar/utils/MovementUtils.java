8c3ca4ee4da2559260a763b4f2c66a9f45f1a4dc
dongha-byun <byunsw4@naver.com>
2022-10-09 20:28:21 +0900

    Fix: 경주 횟수 입력 오류 시, 오류메시지 수정

--- src/main/java/racingcar/utils/MovementUtils.java
+++ src/main/java/racingcar/utils/MovementUtils.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 public class MovementUtils {
     public static int validateOnlyNumber(String input) {
         if(!Pattern.matches("^\\d*$", input)){
-            throw new IllegalArgumentException("");
+            throw new IllegalArgumentException("[ERROR] 시도 횟수는 숫자여야 합니다.");
         }
         return Integer.parseInt(input);
     }
