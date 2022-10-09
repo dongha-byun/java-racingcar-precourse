c1be0fe61f945c3e00d4ed78c8ffe1abc621fb74
dongha-byun <byunsw4@naver.com>
2022-10-09 17:52:54 +0900

    Test: CarName 객체 관련 유효성 검사 테스트케이스 추가

--- src/test/java/racingcar/utils/CarNameParserTest.java
+++ src/test/java/racingcar/utils/CarNameParserTest.java
@@ -2,6 +2,9 @@ package racingcar.utils;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import racingcar.domain.CarName;
+
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class CarNameParserTest {
 
@@ -11,5 +14,19 @@ public class CarNameParserTest {
         String[] names = CarNameParser.parseCarName(carNameStr);
 
         Assertions.assertThat(names.length).isEqualTo(3);
+    }
+
+    @Test
+    void 차_이름_길이_예외_테스트(){
+        assertThatIllegalArgumentException().isThrownBy(
+                () -> CarNameParser.validateCarNameSize("dongHa")
+        );
+    }
+
+    @Test
+    void 띄어쓰기_예외_테스트(){
+        assertThatIllegalArgumentException().isThrownBy(
+                () -> CarNameParser.validateBlankInCarName("dong ")
+        );
     }
 }
